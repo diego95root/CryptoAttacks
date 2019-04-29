c5c4d4cc0286999922f8852dcead7990387caae5
diego95root <minidiego@ono.com>
2019-04-29 18:36:35 +0100

    Set 1 until ex 6

---
+++ set1/hex2base64.py
@@ -0,0 +1,9 @@
+
+def hex2base64(hex):
+    return hex.decode("hex").encode("base64").strip()
+
+if __name__ == "__main__":
+
+    hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+
+    assert "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t" == hex2base64(hex)
